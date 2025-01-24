1d04605ca28b3f982a9678380e2e203c7139f7a2
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-01-24 17:33:03 +0100

    Fixed #15

---
+++ shinyapp/R/portfolio/text_section.R
@@ -0,0 +1,24 @@
+
+
+#' Text Section
+#'
+#' @param content a list to build the section
+#'
+#' @returns a list of tags
+#'
+#' @examples
+
+text_section <- function(content){
+  
+  cat("[text_section] input size =", nrow(values), "\n")
+  
+  # -- return
+  tagList(
+    
+    # -- title
+    h2(class = "mt-5 mb-3", content$title),
+    
+    # -- body
+    eval(parse(text = content$"...")))
+  
+}
