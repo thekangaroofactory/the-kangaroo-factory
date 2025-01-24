ddfc16a3480a3fb76f74c5fd2b2a90ae9fd5c8e0
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-01-24 14:29:35 +0100

    Fixed #10

--- shinyapp/R/portfolio/card_project.R
+++ shinyapp/R/portfolio/card_project.R
@@ -11,49 +11,36 @@
 #'
 #' @examples
 
-
 card_project <- function(x, ns, input, path_portfolio){
   
   cat("[card_project] Build card, project =", x$name, "\n")
   
-  # -- card
+  # -- return project card
   card(
     id = ns(paste0("project_", x$id)),
     class = "border border-light",
     fill = FALSE,
-    full_screen = TRUE,
     
-    # -- card content
-    # adding an explicit card_body with padding 0 otherwise an implicit one
-    # is added with some padding around the image.
+    # -- preview
+    card_image(
+      file = file.path(path_portfolio, paste0("p", x$id), "preview.jpg"),
+      alt = "Project preview image"),
+    
+    # -- summary
     card_body(
-      class = "p-0",
-      
-      # -- dynamic content (preview vs full screen)
-      renderUI({
-        
-        # -- check if card is full screen
-        if(isFALSE(input[[paste0("project_", x$id, "_full_screen")]]))
-          
-          # -- preview
-          tagList(
-            card_image(
-              file = file.path(path_portfolio, paste0("p", x$id), "preview.jpg"),
-              alt = "Project preview image"),
-            card_body(
-              card_title(x$name),
-              p(x$type, br(), x$summary)))
-        
-        else
-          
-          # -- full screen
-          tagList(
-            h1(x$name),
-            p("This is the full screen mode content."),
-            p(x$type, br(), x$summary))
-        
-      }) # renderUI
-    ) # card_body
-  ) # card
+      card_title(x$name),
+      p(x$type, br(), x$summary)),
+    
+    # -- expand
+    card_footer(
+      tooltip(
+        actionButton(inputId = paste0("open_p", x$id),
+                     label = NULL,
+                     icon = icon("up-right-and-down-left-from-center"),
+                     class = "rounded-pill btn-sm btn-outline-light float-end",
+                     onclick = sprintf('Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})', 
+                                       ns("open_project"))),
+        "Open project",
+        placement = "left")))
   
 }
