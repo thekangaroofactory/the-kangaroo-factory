51572911b7c3094e5bbb13de87cfb9cdcdffd583
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-02-16 12:13:03 +0100

    Fixed #46

--- shinyapp/R/profile/profile_links.R
+++ shinyapp/R/profile/profile_links.R
@@ -2,18 +2,21 @@
 
 profile_links <- function(){
   
+  # -- layout
   layout_column_wrap(
     width = "30px",
     fixed_width = TRUE,
     
-    # tooltip(
-    #   tags$a(
-    #     'data-value' = "link_p3_app_shinyio",
-    #     icon(style="font-size: 36px; color: #726d67;", "arrow-up-right-from-square"),
-    #     target = "_blank",
-    #     href ="http://google.com"),
-    #   "See full resume"),
+    # -- portfolio
+    tooltip(
+      tags$a(
+        'data-value' = "link_profile_tkf",
+        icon(style="font-size: 36px; color: #726d67;", "file"),
+        target = "_blank",
+        href = "https://www.thekangaroofactory.com/?user=philippeperet&nav=profile"),
+      "See online version"),
     
+    # -- linkedin
     tooltip(
       tags$a(
         'data-value' = "link_profile_linkedin",
@@ -22,21 +25,23 @@ profile_links <- function(){
         href = "https://www.linkedin.com/in/philippeperet/"),
       "Visit my LinkedIn profile"),
     
+    # -- portfolio
+    tooltip(
+      tags$a(
+        'data-value' = "link_portfolio_tkf",
+        icon(style="font-size: 36px; color: #726d67;", "briefcase"),
+        target = "_blank",
+        href = "https://www.thekangaroofactory.com/?user=philippeperet&nav=portfolio"),
+      "Visit my portfolio"),
+    
+    # -- github
     tooltip(
       tags$a(
         'data-value' = "link_profile_github",
         icon(style="font-size: 36px; color: #726d67;", "github"),
         target = "_blank",
         href = "https://github.com/thekangaroofactory"),
-      "Explore my GitHub repository"),
-    
-    # tooltip(
-    #   tags$a(
-    #     'data-value' = "link_p3_app_shinyio",
-    #     icon(style="font-size: 36px; color: #726d67;", "briefcase"),
-    #     target = "_blank",
-    #     href = "https://www.google.com/"),
-    #   "See my portfolio"),
+      "Explore my GitHub repository")
     
   )
   
