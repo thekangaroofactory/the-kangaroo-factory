6ed706a7e6fd64eb2f788e8b88d5e4502b63d846
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-02-22 18:35:14 +0100

    Cleanup signature

--- shinyapp/R/profile/profile_title.R
+++ shinyapp/R/profile/profile_title.R
@@ -2,21 +2,20 @@
 
 #' Profile Title
 #'
-#' @param title a character string 
-#' @param subtitle an optional character string
+#' @param title a named list of shape list(main = "foo", subtitle = "bar")
 #'
 #' @returns a list of html tags
 #'
 #' @examples
 
-profile_title <- function(title, subtitle = NULL){
+profile_title <- function(title){
   
   # -- return
   tagList(
     
-    h1(tools::toTitleCase(title)),
+    h1(tools::toTitleCase(title$main)),
     
-    if(!is.null(subtitle))
-      p(class = "subtitle", ktools::toupperfirst(subtitle)))
+    if(!is.null(title$subtitle))
+      p(class = "subtitle", ktools::toupperfirst(title$subtitle)))
   
 }
