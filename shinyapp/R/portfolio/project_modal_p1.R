502e23716e75cd61d9385c5bf78a50322c7a59ed
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-09-13 18:52:33 +0200

    Update P1 #82

--- shinyapp/R/portfolio/project_modal_p1.R
+++ shinyapp/R/portfolio/project_modal_p1.R
@@ -16,21 +16,21 @@ project_modal_p1 <- function(path){
     
     # -- Project info section
     layout_column_wrap(class = "mt-5",
-                       value_box(class = "border border-light",
+                       value_box(class = "bg-tkf border border-light",
                                  showcase = icon("chart-line"),
                                  theme = value_box_theme(bg = "#726d67", fg = "#d9cec2"),
                                  title = "Project Type",
                                  value = "Dashboard",
                                  p("Web application & API containers deployed on the cloud")),
                        
-                       value_box(class = "border border-light",
+                       value_box(class = "bg-tkf border border-light",
                                  showcase = icon("hourglass"),
                                  theme = value_box_theme(bg = "#726d67", fg = "#d9cec2"),
                                  title = "Workload",
                                  value = "40 days",
                                  p("Excl. AI model training")),
                        
-                       value_box(class = "border border-light",
+                       value_box(class = "bg-tkf border border-light",
                                  showcase = icon("unlock"),
                                  theme = value_box_theme(bg = "#726d67", fg = "#d9cec2"),
                                  title = "Visibility",
@@ -42,9 +42,9 @@ project_modal_p1 <- function(path){
     # -- About section
     h2(class = "section",
        "About the Project"),
-    p("The Rain Forecast Dashboard is part of a project that started in 2020.", br(),
-      "An AI model (Machine Learning) was then trained on a 140k dataset provided by the Australian Government BOM (Bureau of Meteorology)."),
-    p("Since then, weather observation data have been collected continuously to keep monitoring the rain predictions."),
+    p("This dashboard is part of a project that started in 2020.", br(),
+      "It's built around a Machine Learning model (AI) that was trained on a 140k dataset provided by the Australian Government BOM (", a(href = "https://www.bom.gov.au/", target = "_blank", "Bureau of Meteorology"), ")."),
+    p("Weather observation data have been collected continuously to keep monitoring the rain predictions."),
     
     
     # -- Features section
@@ -53,24 +53,24 @@ project_modal_p1 <- function(path){
     layout_column_wrap(
       card(class = "border border-light",
            card_header("Data engineering"),
-           p("Observations are collected from an external data source, then cleaned, prepared and stored into a database."),
+           p("Observations are collected from an external data source, cleaned, prepared and stored into a database."),
            p("The prepared data (observations & predictions) are served by an API to support the dashboard.")),
       card(class = "border border-light",
-           card_header("Observation dashboard"),
+           card_header("Observations"),
            card_image(
              class = "p-3",
              src = "project_media/dashboard_observations.jpg",
              alt = "Project image"),
-           p("It allows to explore the collected weather observations, using filters to select specific date range."),
-           p("Dedicated data visualizations have been developped with a highly graphical approach.")),
+           p("It's possible to explore the collected weather observations, using filters to select specific date range."),
+           p("Data visualizations have been developped with a highly graphical approach (here rainfall).")),
       card(class = "border border-light",
-           card_header("Prediction dashboard"),
+           card_header("Predictions"),
            card_image(
              class = "p-3",
              src = "project_media/dashboard_predictions.jpg",
              alt = "Project image"),
-           p("This part of the dashboard displays the latest predictions and monitors the ML model performances."),
-           p("A set of MLOps KPIs have been implemented with decicated code to fit with the dashboard design."))),
+           p("The dashboard displays the latest predictions and monitors the ML model performances."),
+           p("A set of MLOps KPIs have been implemented."))),
     
     
     # -- Background section
@@ -79,16 +79,16 @@ project_modal_p1 <- function(path){
     
     # -- Architecture sub-section
     h3("Architecture"),
-    p("The project architecture has different layers to encapsulate dedicated tasks inside independant components."),
+    p("The project architecture has different layers to encapsulate tasks inside independant components."),
     p("Layers"),
     tags$ul(
       tags$li("A data layer to store the database & other resources (raw files, schemas, models)"),
-      tags$li("An API layer to perform data engineering operations through individual components (collection, cleaning, transformation, prediction)"),
-      tags$li("Finally, the dashboard layer itself with a server / client web application to get the data visualization computations done in the background")),
+      tags$li("An API layer to perform data engineering operations (collect, clean, transform, predict)"),
+      tags$li("The dashboard layer itself with a server / client web application (visualization computations done in the background)")),
     
     # -- Technical stack sub-section
     card(
-      class = "bg-contrast section",
+      class = "bg-tkf section",
       h3("Technical Stack"),
       layout_column_wrap(
         card(card_header("Dashboard"),
@@ -117,17 +117,17 @@ project_modal_p1 <- function(path){
     layout_column_wrap(
       card(class = "border border-light",
            card_header("API"),
-           p("The API is delivered as a Docker container deployed on GCP.", br(),
+           p("The API is delivered as a Docker container (GCP).", br(),
            "The underlying PostgreSQL database is hosted on Supabase.")),
       card(class = "border border-light",
            card_header("Dashboard"),
-           p("The Dashboard web application is also delivered as a Docker container deployed on GCP."))),
+           p("The Dashboard web application is also delivered as a Docker container (GCP)."))),
     
     
     # -- Document section
     card(class = "border border-light",
-         card_header("Project documentation"),
-         p("Documentation has also been delivered to support communication about the project."),
+         card_header("Documentation"),
+         p("Communication supports have also been delivered."),
          accordion(
            open = FALSE,
            accordion_panel(
@@ -138,16 +138,9 @@ project_modal_p1 <- function(path){
     
     # -- Links section
     card(
-      class = "bg-contrast section",
+      class = "bg-tkf section",
       h2("Links"),
       layout_column_wrap(
-        card(card_header("Dashboard"),
-             p("A public instance of the web application is accessible (cold start may create a delay at startup)."),
-             tags$a(
-               'data-value' = "link_p1_dashboard_gcp",
-               "Rain Forecast Dashboard",
-               target = "_blank",
-               href = "https://rain-forecast-dashboard-902168338454.europe-west9.run.app/")),
         card(card_header("Repository"),
              p("The GitHub", icon("github"), "repository of the API."),
              tags$a(
