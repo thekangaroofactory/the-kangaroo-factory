ac20021a21cf4db5585067bfd2b00719178ab221
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-09-13 10:23:24 +0200

    .

--- app.R
+++ app.R
@@ -1,6 +1,11 @@
 
+# -- source app files
+cat("Source app files \n")
 source("shinyapp/global.R")
+source("shinyapp/server.R")
+source("shinyapp/ui.R")
 
 # -- create shiny app
+cat("Start app \n")
 shinyApp(ui = ui(), 
          server = server())
