34ca9674887168bf6b3257c5dd6015e8775fe594
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-11-27 16:48:45 +0100

    Cleanup #107

--- shinyapp/R/contact/contact_ui.R
+++ shinyapp/R/contact/contact_ui.R
@@ -2,30 +2,16 @@
 
 contact_ui <- function(id){
   
-  card(
-    class = "border-radius tkf-bg-camel color-dark p-3 mt-5",
+  div(
+    class = "section",
     
-    # -- layout
-    layout_columns(
-      col_widths = c(4, 8),
-      
-      # -- text
-      p("Contact me with an email or a message on LinkedIn."),
-      
-      # -- cards
-      layout_column_wrap(
-        class = "mt-5",
-        
-        card(
-          class = "border border-light",
-          card_header("Email"),
-          div("Drop me an", a(href="mailto:philippe.peret@hotmail.com", "email"))),
-        
-        card(
-          class = "border border-light",
-          card_header("LinkedIn"),
-          div("Visit my", a(href="https://www.linkedin.com/in/philippeperet/", target = "_blank", "profile"))),
-        
-      )))
+    # -- email
+    p("Send me an", a(href = "mailto:philippe.peret@hotmail.com", "email")), br(),
+    
+    # -- linkedin
+    actionButton(
+      inputId = "linkedin",
+      label = "linkedin",
+      onclick = paste0("window.open('", "https://www.linkedin.com/in/philippeperet/", "', '_blank')")))
   
 }
