f2223f34d80ca5c330fac095d4e16519fd7ecc08
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-03-27 19:58:17 +0100

    Fixed #124

--- shinyapp/R/portfolio/card_project.R
+++ shinyapp/R/portfolio/card_project.R
@@ -29,20 +29,19 @@ card_project <- function(x, ns, input, path_portfolio){
     # -- summary
     card_body(
       card_title(x$name),
-      p(x$type, br(), x$summary)),
+      # div(class = "badge tag tkf-bg-accent color-dark", x$type),
+      
+      span(
+      lapply(unlist(strsplit(x$type, split = " ")), div, class = "badge tag tkf-bg-accent color-dark")),
+      
+      p(x$summary)),
     
     # -- expand
     card_footer(
-      tooltip(
-        # -- button
-        # add gtag to trigger google analytics js
         actionButton(inputId = paste0("open_p", x$id),
-                     label = NULL,
-                     icon = icon("up-right-and-down-left-from-center"),
+                     label = "view project",
                      class = "rounded-pill btn-sm float-end gtag",
                      onclick = sprintf('Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})', 
-                                       ns("open_project"))),
-        "Open project",
-        placement = "left")))
+                                       ns("open_project")))))
   
 }
