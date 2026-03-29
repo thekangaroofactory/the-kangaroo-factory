49ca11aa7f19e8c07c9d24829285d5b043b5e040
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-03-29 11:50:36 +0200

    Impacts #109

--- shinyapp/R/blog/blog_ui.R
+++ shinyapp/R/blog/blog_ui.R
@@ -5,6 +5,7 @@ blog_ui <- function(){
   
   # -- return
   tags$iframe(
+    class = "blog",
     src = "https://thekangaroofactory.github.io/the-kangaroo-factory-blog/",
     scrolling = 'yes')
   
--- shinyapp/R/wiki/wiki_ui.R
+++ shinyapp/R/wiki/wiki_ui.R
@@ -5,6 +5,7 @@ wiki_ui <- function(){
   
   # -- return
   tags$iframe(
+    class = "blog",
     src = "https://thekangaroofactory.github.io/the-kangaroo-factory-wiki/",
     scrolling = 'yes')
   
